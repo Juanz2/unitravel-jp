9814997304b5fcbea22bb4462b41b42ca7eb1b7f
Juan Pablo Zabala Hernandez <juanz@Juans-MacBook-Pro.local>
2022-03-13 18:04:41 -0500

    Configuracion inicial

---
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/Persistencia.java
@@ -0,0 +1,12 @@
+package co.edu.uniquindio.unitravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Persistencia {
+
+    public static void main (String [] args){
+        SpringApplication.run(Persistencia.class, args);
+    }
+}
