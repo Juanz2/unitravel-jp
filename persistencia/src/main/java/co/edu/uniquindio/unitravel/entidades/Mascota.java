1bdd794ab20198eb8545163b0cf16bad21f85912
julieth <julieth@example.com>
2022-04-17 22:22:43 -0500

    Se crea la clase mascota

---
+++ persistencia/src/main/java/co/edu/uniquindio/unitravel/entidades/Mascota.java
@@ -0,0 +1,14 @@
+package co.edu.uniquindio.unitravel.entidades;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Mascota {
+    private String nombre;
+    private String raza;
+}
